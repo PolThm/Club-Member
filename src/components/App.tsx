4b04519d8786e7b57eef9a4877722392f3e2bc0e
PolThm <pol.thomas56@gmail.com>
2020-10-12 13:06:18 +0200

    Tests WIP

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,12 +4,10 @@ import './App.css';
 import Header from "./Header";
 import EnterEmailSection from "./EnterEmailSection";
 import Results from "./Results";
-import ExampleComponent from "./ExampleComponent";
 
 const App = () => {
   return (
     <GDCContextProvider>
-      <ExampleComponent/>
       <Header/>
       <div className="md:flex">
         <EnterEmailSection/>
--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import {unmountComponentAtNode } from "react-dom";
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
-import {act} from "react-dom/test-utils";
+import { shallow, configure, mount } from 'enzyme';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, fireEvent } from '@testing-library/react'
-import {GDCContextProvider} from "../../context";
+import { render, screen, fireEvent, act } from '@testing-library/react'
+import {GDCContextProvider, useGDCContent} from "../../context";
 import App from '../App'
 import FormLine from "../FormLine";
 import Header from "../Header";
 import Result from "../Result";
 import Results from "../Results";
 import EnterEmailSection from "../EnterEmailSection";
-import ExampleComponent from "../ExampleComponent";
 
 configure({adapter: new Adapter()});
 
@@ -26,6 +23,44 @@ const renderWithContext = (
     )
   }
 }
+
+// describe ('FetchDB', () => {
+//   const TestComponent = () => {
+//     const {dataBase, submitEmail} = useGDCContent();
+//
+//     return (
+//       <>
+//         <div data-testid="value">{dataBase.toString()}</div>
+//         <button onClick={submitEmail}>submitEmail</button>
+//       </>
+//     )
+//   }
+//   const wrapper = mount(
+//     <GDCContextProvider>
+//       <TestComponent />
+//     </GDCContextProvider>
+//   )
+//
+//   expect(wrapper.find('[data-testid="value"]').text().toEqual(["isLoading"]))
+// })
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve(
+        ["nicolas@outlook.fr", "celine@outlook.fr", "kevin@gmail.com", "sylvie@gmail.com"]
+      )
+  })
+)
+
+describe('FetchDB', () => {
+  it ('loads FormLine on mount', async () => {
+    await act(async () => render(<App />));
+
+    const submitButton = await screen.getByRole('button', {name: 'Submit'})
+    expect(submitButton).toBeInTheDocument()
+  });
+});
 
 // describe('FormLine', () => {
 //   it('submit button is displayed', async () => {
@@ -44,27 +79,3 @@ const renderWithContext = (
 //     expect(screen.getByTestId('input-error')).toHaveTextContent("Email required")
 //   })
 // });
-
-
-describe('fetchDB', () => {
-  it('fetches data from server when server returns a successful response', done => { // 1
-    const mockSuccessResponse = {};
-    const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
-    const mockFetchPromise = Promise.resolve({ // 3
-      json: () => mockJsonPromise,
-    });
-    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise); // 4
-
-    const wrapper = shallow(<ExampleComponent />); // 5
-
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith('http://www.json-generator.com/api/json/get/bQrQrLgMEi?indent=2');
-
-    process.nextTick(() => { // 6
-      expect(wrapper.instance()('database')).toEqual(true)
-
-      global.fetch.mockClear(); // 7
-      done(); // 8
-    });
-  });
-});
